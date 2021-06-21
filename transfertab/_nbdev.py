a67cf40f5c003472ce2f725d79d28d3884aa05ac
JoeRishon <joerishon@gmail.com>
2021-06-21 14:43:21 +0530

    updated ExtractUtils

--- transfertab/_nbdev.py
+++ transfertab/_nbdev.py
@@ -4,6 +4,7 @@ __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"extract_meta_from_learner": "00_ExractUtils.ipynb",
          "extract_meta_from_df": "00_ExractUtils.ipynb",
+         "CustomJSONizer": "00_ExractUtils.ipynb",
          "extractembeds": "00_ExractUtils.ipynb",
          "TabTransfer": "01_transfer.ipynb"}
 
